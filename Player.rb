06526b9c3bf37302aefaba16f4af822f99b39404
Dan Blakeman <DanBlakeman@users.noreply.github.com>
2015-03-09 22:30:36 +0000

    minor refactor of prompt to make it universal within game class

--- Player.rb
+++ Player.rb
@@ -11,15 +11,23 @@ class Player
   def initialize()
     @@player_count += 1
     user_create_player
+
+  end
+
+  def user_prompt
+    print "=>"
   end
 
   def user_create_player
     puts "PLAYER #{@@player_count}, What's your name?"
+    user_prompt
     player_name = gets.chomp
     puts "PLAYER #{@@player_count}, What's your mark?"
+    user_prompt
     player_mark = gets.chomp
     while player_mark.length > 2 || player_mark.length < 1
       puts "A mark must be either one or two characters long! Please enter a new mark:"
+      @user_prompt
       player_mark = gets.chomp
     end
     player_mark = " "+player_mark if player_mark.length < 2
--- game.rb
+++ game.rb
@@ -11,81 +11,21 @@ require_relative 'Player'
 
 class Game
 
-  attr_reader :current_player
-
-  def initialize(board=Board.new)
-    want_to_load
+  def initialize()
+    want_to_load_existing_game
     user_create_players
     @current_player, @other_player = [@current_player, @other_player].shuffle
-    @board = board #Can remove and place in play method?
   end
 
-  def want_to_load
-     puts "If you'd like to load a game type 'load' and hit return, else press any key and return to continue!"
-     print "=>"
-     wishes_to_load = gets.chomp.downcase
-     return load_game if wishes_to_load == 'load'
-     puts "OK, great! Let's get down to business!"
-  end
-
-  def user_create_players
-     @current_player = Player.new
-     @other_player = Player.new
-  end
-
-  def get_input
-      puts "Which row would you like to place a counter?"
-      print "=>"
-      @row = gets.chomp
-      return save_game if @row== "save"
-      return load_game if @row == "load"
-      puts "Which column would you like to place a counter?"
-      print "=>"
-      @column = gets.chomp
-      return save_game if @column == "save"
-      return load_game if @column == "load"
-      @row = @row.to_i
-      @column = @column.to_i
-      if !((1..3).include?@row) || !((1..3).include?@column)
-        puts "You aint making sense, try again"
-        get_input
-      end
-      if @board.grid[@row-1][@column-1].mark != @board.grid[@row-1][@column-1].default_mark
-        puts "Try again, that spots taken!"
-        get_input
-      end
-  end
-
-  # Why can i not create a prompt variable?
-
-  def save_game
-    save_data = YAML::dump(self)
-    puts "Please enter a name for your save game:"
-    print "=>"
-    @filename = gets.chomp
-    File.open("./saved/#{@filename}", "w") { |file| file.write(save_data)}
-    puts "Saved"
-    get_input
-  end
-
-  def load_game
-    saved_games = Dir["./saved/*"]
-    saved_games.each_with_index { |file, index| puts "#{index+1}. #{file[8..-1]}"}
-    puts "Enter the number of the game you would like to load:"
-    file_to_load = gets.chomp.to_i
-    file_to_load = saved_games[file_to_load-1]
-    loaded_data = ""
-    File.open("#{file_to_load}", "r") { |file| loaded_data =file.read}
-    loaded_game = YAML::load(loaded_data)
-    loaded_game.play
-  end
-
-  def play
+    def play
+    @board = Board.new
     first_loop = true
     while true
       puts "#{@current_player.name}'s turn! (You're counter is #{@current_player.mark})"
-      @board.show if first_loop
-      puts "If at anytime you'd like to save or load a game, simply type 'save' or 'load'!" if first_loop
+      if first_loop
+        puts "If at anytime you'd like to save or load a game, simply type 'save' or 'load'!"
+        @board.show
+      end
       first_loop = false
       get_input
       @board.mark_cell(@row, @column, @current_player.mark)
@@ -111,6 +51,72 @@ class Game
      end
   end
 
+  def prompt
+    print "=>"
+  end
+
+  private
+
+  def want_to_load_existing_game
+     puts "If you'd like to load a game type 'load' and hit return, else press any key and return to continue!"
+     print "=>"
+     wishes_to_load = gets.chomp.downcase
+     return load_game if wishes_to_load == 'load'
+     puts "OK, great! Let's get down to business!"
+  end
+
+  def user_create_players
+     @current_player = Player.new()
+     @other_player = Player.new()
+  end
+
+  def get_input
+      puts "Which row would you like to place a counter?"
+      prompt
+      @row = gets.chomp
+      return save_game if @row== "save"
+      return load_game if @row == "load"
+      puts "Which column would you like to place a counter?"
+      prompt
+      @column = gets.chomp
+      return save_game if @column == "save"
+      return load_game if @column == "load"
+      @row = @row.to_i
+      @column = @column.to_i
+      if !((1..3).include?@row) || !((1..3).include?@column)
+        puts "You aint making sense, try again"
+        get_input
+      end
+      if @board.grid[@row-1][@column-1].mark != @board.grid[@row-1][@column-1].default_mark
+        puts "Try again, that spots taken!"
+        get_input
+      end
+  end
+
+  # Why can i not create a prompt variable?
+
+  def save_game
+    save_data = YAML::dump(self)
+    puts "Please enter a name for your save game:"
+    prompt
+    @filename = gets.chomp
+    File.open("./saved/#{@filename}", "w") { |file| file.write(save_data)}
+    puts "Saved"
+    get_input
+  end
+
+  def load_game
+    saved_games = Dir["./saved/*"]
+    saved_games.each_with_index { |file, index| puts "#{index+1}. #{file[8..-1]}"}
+    puts "Enter the number of the game you would like to load:"
+    prompt
+    file_to_load = gets.chomp.to_i
+    file_to_load = saved_games[file_to_load-1]
+    loaded_data = ""
+    File.open("#{file_to_load}", "r") { |file| loaded_data =file.read}
+    loaded_game = YAML::load(loaded_data)
+    loaded_game.play
+  end
 
   def switch_players
     @current_player, @other_player = @other_player, @current_player
@@ -147,4 +153,4 @@ end
 
  game = Game.new()
  game.play
- #Board.new.show
+
